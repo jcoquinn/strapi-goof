25d9e10d9ee2030e6dfa59cca1076e58e4ef291a
jcoquinn <9020588-jcoquinn@users.noreply.gitlab.com>
2025-06-20 10:32:24 -0400

    tweaks

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,9 @@
-export default () => ({});
+export default ({ env }) => ({
+    'users-permissions': {
+        config: {
+            jwt: {
+                expiresIn: '8h',
+            },
+        },
+    },
+});
